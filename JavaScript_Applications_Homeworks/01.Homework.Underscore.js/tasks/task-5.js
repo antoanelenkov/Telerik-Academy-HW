8759e844ee006039ab81f88d0eb3745311b9eb11
antoanelenkov <antoanelenkov@gmail.com>
2015-08-31 13:28:49 +0300

    last two tasks of underscore.js HW are ready

--- JavaScript_Applications_Homeworks/01.Homework.Underscore.js/tasks/task-5.js
+++ JavaScript_Applications_Homeworks/01.Homework.Underscore.js/tasks/task-5.js
@@ -1,16 +1,46 @@
-/* 
- Create a function that:
+/*   Create a function that:
  *   **Takes** an array of animals
  *   Each animal has propeties `name`, `species` and `legsCount`
  *   **finds** and **prints** the total number of legs to the console in the format:
  *   "Total number of legs: TOTAL_NUMBER_OF_LEGS"
  *   **Use underscore.js for all operations**
  */
+
 var _=require('./node_modules/underscore/underscore.js');
+
+var animal = (function () {
+    var animal = {
+        init: function (name, species, legsCount) {
+            this.name = name;
+            this.species = species;
+            this.legsCount = legsCount;
+
+            return this;
+        }
+    };
+    return animal;
+})();
 
 function solve(){
     return function (animals) {
+        var allLegs=0;
+
+        _.chain(animals)
+            .each(function(animal){
+                allLegs+=animal.legsCount*1;
+            })
+
+        console.log("Total number of legs: "+allLegs)
     };
 }
+
+var animal1 = Object.create(animal).init('Minkov', 'Mosquito', 2);
+var animal2 = Object.create(animal).init('Doncho', 'Mosquito', 2);
+var animal3 = Object.create(animal).init('Komara', 'Mosquito', 4);
+var animal4 = Object.create(animal).init('aaaa', 'dog', 4);
+var animals = [animal1, animal2, animal3];
+
+solve()(animals);
+
 
 module.exports = solve;
--- JavaScript_Applications_Homeworks/01.Homework.Underscore.js/tasks/task-6.js
+++ JavaScript_Applications_Homeworks/01.Homework.Underscore.js/tasks/task-6.js
@@ -13,7 +13,41 @@ var _=require('./node_modules/underscore/underscore.js');
 
 function solve(){
     return function (books) {
+        var booksGroupedByAuthor= _.chain(books)
+            .groupBy(function(book){
+               return book.author.firstName+' '+book.author.lastName;
+            })
+            .value();
+
+        var count=0;
+        var authors=[];
+        var sorted= _.chain(booksGroupedByAuthor)
+            .each(function(books){
+                if(count<books.length){
+                    count=books.length
+                }
+            })
+           .each(function(books){
+               if(books.length===count){
+                   authors.push({fullName:books[0].author.firstName+' '+books[0].author.lastName});
+               }
+           })
+
+        _.chain(authors)
+            .sortBy('fullName')
+            .each(function(author){
+                console.log(author.fullName)
+            })
     };
 }
+
+var book1 = {title:'peese niunsa sivo',author:{firstName:'authorFirstName1',lastName:'authorLastName1'}};
+var book2 = {title:'peese niunsa sivo2',author:{firstName:'authorFirstName1',lastName:'authorLastName1'}};
+var book3 = {title:'peese niunsa sivo3',author:{firstName:'authorFirstName3',lastName:'authorLastName3'}};
+var book4 = {title:'peese niunsa sivo4',author:{firstName:'authorFirstName3',lastName:'authorLastName3'}};
+var book5 = {title:'peese niunsa sivo5',author:{firstName:'authorFirstName5',lastName:'authorLastName5'}};
+var books = [book1, book2, book3,book4,book5];
+
+solve()(books);
 
 module.exports = solve;
