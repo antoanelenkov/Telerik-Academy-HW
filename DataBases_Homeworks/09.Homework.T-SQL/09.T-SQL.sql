aaa7abb4673238b3b391a15c1aa7d99ed13cd0f2
antoanelenkov <antoanelenkov@gmail.com>
2015-10-07 01:21:34 +0300

    last 4 tasks next year ;)

--- DataBases_Homeworks/09.Homework.T-SQL/09.T-SQL.sql
+++ DataBases_Homeworks/09.Homework.T-SQL/09.T-SQL.sql
@@ -1,3 +1,4 @@
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --1. Create a database with two tables: Persons(Id(PK), FirstName, LastName, SSN) and Accounts(Id(PK), PersonId(FK), Balance).
 --Insert few records for testing.
 --Write a stored procedure that selects the full names of all persons.
@@ -45,6 +46,9 @@ GO
 EXEC usp_FullNameOfAllPersons
 GO
 
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --2. Create a stored procedure that accepts a number as a parameter and returns all persons who have more money in their accounts than the supplied number.
 
 CREATE PROCEDURE usp_PersonsWithMoreMoneyThanInputValue(@money money=1000)
@@ -58,6 +62,9 @@ EXEC usp_PersonsWithMoreMoneyThanInputValue 10
 EXEC usp_PersonsWithMoreMoneyThanInputValue 
 GO
 
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --3. Create a function that accepts as parameters – sum, yearly interest rate and number of months.
 --It should calculate and return the new sum.
 --Write a SELECT to test whether the function works as expected.
@@ -73,6 +80,9 @@ GO
 SELECT Balance, dbo.ufn_CalculateMoney(Balance,0.5,12) AS [InterestRate] FROM Accounts
 GO
 
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --4. Create a stored procedure that uses the function from the previous example to give an interest to a person's account for one month.
 --It should take the AccountId and the interest rate as parameters.
 
@@ -93,6 +103,8 @@ EXEC usp_AddInterestToPerson 10, @sum
 GO
 
 
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --5. Add two more stored procedures WithdrawMoney(AccountId, money) and DepositMoney(AccountId, money) that operate in transactions.
 
 CREATE PROCEDURE usp_WithdrawMoney(@accountId int,@sum money) 
@@ -116,6 +128,9 @@ GO
 EXEC usp_DepositMoney 1,1000
 GO
 
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --6.Create another table – Logs(LogID, AccountID, OldSum, NewSum).
 --Add a trigger to the Accounts table that enters a new entry into the Logs table every time the sum on an account changes.
 
@@ -143,5 +158,23 @@ Update Accounts
 set Balance=100000
 WHERE Id=10
 
+--SOME OTHER TIME. When evaluating my HW, be honest, if you have done this tasks, don't give me all points ;)
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+7. Define a function in the database TelerikAcademy that returns all Employee''s names (first or middle or last name) and all town''s names that are comprised of given set
+of letters. Example 'oistmiahf' will return 'Sofia', 'Smith', … but not 'Rob' and 'Guy'.
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+8. Using database cursor write a T-SQL script that scans all employees and their addresses and prints all pairs of employees that live in the same town.
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+9. * Write a T-SQL script that shows for each town a list of all employees that live in it. Sample output:
+			Sofia -> Svetlin Nakov, Martin Kulov, George Denchev
+			Ottawa -> Jose Saraiva
+			
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+10. Define a .NET aggregate function StrConcat that takes as input a sequence of strings and return a single string that consists of the input strings separated by ','. 
+For example the following SQL statement should return a single string:
+			SELECT StrConcat(FirstName + ' ' + LastName)
+			FROM Employees
 
 
