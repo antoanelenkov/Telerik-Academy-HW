952310ad1cfb776aa8b768a5a21162c854659117
antoanelenkov <antoanelenkov@gmail.com>
2015-10-08 01:16:18 +0300

    SQLITE part of HW is ready. All HW is done

--- DataBases_Homeworks/10.Homework.ADO.NET/SQLITE/Program.cs
+++ DataBases_Homeworks/10.Homework.ADO.NET/SQLITE/Program.cs
@@ -9,14 +9,43 @@
     {
         static void Main(string[] args)
         {
-            string connectionStringForMySql = ConfigurationManager.ConnectionStrings["BooksMySql"].ConnectionString;
-
+            var connectionString = @"Data Source=..\..\db\SQLiteBooks; Version=3";
             /*Download and install MySQL database, MySQL Connector/ Net(.NET Data Provider for MySQL) +MySQL Workbench 
             GUI administration tool.
             Create a MySQL database to store Books(title, author, publish date and ISBN).
             Write methods for listing all books, finding a book by name and adding a book.
             10. Re-implement the previous task with SQLite embedded DB (see http://sqlite.phxsoftware.com).*/
+            GetBooks(connectionString);
+            InsertBook(connectionString);
+        }
 
+        private static void InsertBook(string connectionString)
+        {
+            SQLiteConnection dbCon = new SQLiteConnection(connectionString);
+            dbCon.Open();
+
+            var command = new SQLiteCommand("INSERT INTO books(Title,Author, PublishDate, ISBN) VALUES (@title,@author,@publishDate,@isbn)", dbCon);
+            command.Parameters.AddWithValue("@title", "New Book");
+            command.Parameters.AddWithValue("@author", "New Author");
+            command.Parameters.AddWithValue("@publishDate", DateTime.Now);
+            command.Parameters.AddWithValue("@isbn", "2324-324-34");
+
+            command.ExecuteNonQuery();
+        }
+
+        private static void GetBooks(string connectionString)
+        {
+            SQLiteConnection dbCon = new SQLiteConnection(connectionString);
+            dbCon.Open();
+
+            SQLiteCommand command = new SQLiteCommand("SELECT * FROM Books", dbCon);
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine("Title: {0}, Author: {1} ISBN: {2}, published on: {3}",
+                       reader["Title"], reader["Author"], reader["ISBN"], ((DateTime)reader["PublishDate"]));
+            }
         }
     }
 }
