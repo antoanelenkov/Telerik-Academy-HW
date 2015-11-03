3a6d71578877ab4e751b8c2626d8ea08d0ab077a
antoanelenkov <antoanelenkov@gmail.com>
2015-11-03 22:06:30 +0200

    first task of Hw is started.

---
+++ WebServices_&_Cloud_Homeworks/01.Homework.WebApi/01.StudentSystem/StudentSystem.Services/Models/StudentModel.cs
@@ -0,0 +1,21 @@
+﻿namespace StudentSystem.Services.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class StudentModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        public int Level { get; set; }
+    }
+}
