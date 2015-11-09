61ac798726e0d788af131b4becc23289e901e9d9
antoanelenkov <antoanelenkov@gmail.com>
2015-11-09 15:37:11 +0200

    implemented priority queue with Heap.

---
+++ DSA_Homeworks/05.Homework.AdvancedDataStructures/AdvancedDataStructures/MyPriorityQueue.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AdvancedDataStructures
+{
+    class MyPriorityQueue<T> where T : IComparable<T>
+    {
+        private const int InitialSize = 8;
+        public T[] heapElements;
+
+        public MyPriorityQueue()
+        {
+            this.heapElements = new T[InitialSize];
+            this.Size = 0;
+        }
+
+        public int Size { get; set; }
+
+        public void Enqueue(T element)
+        {
+            if (this.Size == this.heapElements.Length)
+            {
+                this.Resize();
+            }
+
+            this.AddRecursive(element, this.Size);
+            this.Size++;
+        }
+
+        private void Resize()
+        {
+            var size = this.heapElements.Length;
+            var newCollection = new T[size * 2];
+
+            for (int i = 0; i < this.heapElements.Length; i++)
+            {
+                newCollection[i] = this.heapElements[i];
+            }
+
+            this.heapElements = newCollection;
+        }
+
+        public T Dequeue()
+        {
+            var elementToDelete = this.heapElements[0];
+
+            this.heapElements[0] = this.heapElements[Size - 1];
+            this.Size--;
+            this.DeleteRecursive(this.heapElements[0], 0);
+
+            return elementToDelete;
+        }
+
+        public IEnumerable<T> All()
+        {
+            var collection = new T[this.Size];
+
+            for (int i = 0; i < this.Size; i++)
+            {
+                collection[i] = this.heapElements[i];
+            }
+
+            return collection;
+        }
+
+        public int Count()
+        {
+            return this.Size;
+        }
+
+        private void AddRecursive(T element, int position)
+        {
+            var previousPosition = 0;
+
+            if (position == 0)
+            {
+                this.heapElements[position] = element;
+                return;
+            }
+            else if (position == 1)
+            {
+                previousPosition = 0;
+            }
+            else if (position % 2 != 0)
+            {
+                previousPosition = position / 2;
+            }
+            else
+            {
+                previousPosition = position / 2 - 1;
+            }
+
+            if (this.heapElements[previousPosition].CompareTo(element) < 0)
+            {
+                var temp = this.heapElements[previousPosition];
+                this.heapElements[previousPosition] = element;
+                this.heapElements[position] = temp;
+
+                AddRecursive(element, previousPosition);
+            }
+            else
+            {
+                this.heapElements[position] = element;
+            }
+
+            return;
+        }
+
+        private void DeleteRecursive(T element, int position)
+        {
+            var nextLeftIndex = position == 0 ? 1 : position * 2 + 1;
+            var nextRightIndex = position == 0 ? 2 : position * 2 + 2;
+
+            if (nextLeftIndex >= this.Size && nextRightIndex >= this.Size)
+            {
+                return;
+            }
+
+            var leftNode = this.heapElements[nextLeftIndex];
+            var rightNode = this.heapElements[nextRightIndex];
+
+            var nextIndex = this.heapElements[nextLeftIndex]
+                .CompareTo(this.heapElements[nextRightIndex]) > 0 ? nextLeftIndex : nextRightIndex;
+
+            if (this.heapElements[nextIndex].CompareTo(element) > 0)
+            {
+                var temp = this.heapElements[nextIndex];
+                this.heapElements[nextIndex] = element;
+                this.heapElements[position] = temp;
+
+                DeleteRecursive(element, nextIndex);
+            }
+
+            return;
+        }
+    }
+}
---
+++ DSA_Homeworks/05.Homework.AdvancedDataStructures/AdvancedDataStructures/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+
+namespace AdvancedDataStructures
+{
+    class Program
+    {
+        /*Implement a class PriorityQueue<T> based on the data structure "binary heap".
+        Write a program to read a large collection of products(name + price) and efficiently find the first 20 products in the price range[a…b].
+        Test for 500 000 products and 10 000 price searches.
+        Hint: you may use OrderedBag<T> and sub-ranges.
+        Write a program that finds a set of words (e.g. 1000 words) in a large text(e.g. 100 MB text file).
+        Print how many times each word occurs in the text.
+        Hint: you may find a C# trie in Internet.*/
+        static void Main(string[] args)
+        {
+            var priorityQueue = new MyPriorityQueue<int>();
+
+            priorityQueue.Enqueue(1);
+            priorityQueue.Enqueue(3);
+            priorityQueue.Enqueue(18);
+            priorityQueue.Enqueue(4);
+            priorityQueue.Enqueue(105);
+            priorityQueue.Enqueue(213);
+
+            var last = priorityQueue.Dequeue();
+
+            Console.WriteLine("Dequed: " + last);
+            Console.WriteLine("Size of queue: " + priorityQueue.Count());
+            Console.WriteLine("All elements: \n" + String.Join(", ", priorityQueue.All()));
+        }
+    }
+}
